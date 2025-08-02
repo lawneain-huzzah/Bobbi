d0a0313811af2e9a96cfc4e7d79b669e50ee8350
La Wun <la_wun_eain_2028@mgs.sch.edu.sg>
2025-08-02 14:31:27 +0800

    Initial Commit

---
+++ Bobbi/BobbiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobbiApp.swift
+//  Bobbi
+//
+//  Created by La Wun Eain  on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BobbiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
